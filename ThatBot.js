720a65d9cbc0c9a7467a537bab39a18e996beccd
faizal101 <faizal_10@live.co.uk>
2018-04-10 17:28:49 +0100

    coin flip command added

--- ThatBot.js
+++ ThatBot.js
@@ -12,7 +12,8 @@ const bot = new Commando.Client({
 
 bot.registry
     .registerGroups([
-        ["misc", "Miscellanous, where commands don't have a place to go"]
+        ["misc", "Miscellanous, where commands don't have a place to go"],
+        ["random", "Random, where all the rng happens "]
     ])
     .registerDefaultGroups()
 		.registerDefaultTypes()
