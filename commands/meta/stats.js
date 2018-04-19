b463acabeb9792c4b0dcecdd0d54bc216227dd64
faizal101 <faizal_10@live.co.uk>
2018-04-19 14:08:08 +0100

    added the stats command
    it’s simple for now

--- commands/meta/stats.js
+++ commands/meta/stats.js
@@ -1,4 +1,5 @@
 const { Command } = require("discord.js-commando");
+const Discord = require("discord.js")
 
 module.exports = class ReplyCommand extends Command {
     constructor(client) {
@@ -12,6 +13,13 @@ module.exports = class ReplyCommand extends Command {
     }
 
     run(msg) {
-        return msg.say("A bot made by Kuuhaku#4503");
+      const embed = new Discord.RichEmbed()
+      .setAuthor(this.client.user.tag, this.client.user.avatarURL)
+      .setColor(0xEC40DF)
+      .setFooter(`Requested by: ${msg.author.tag}`, `${msg.author.avatarURL}`)
+      .setTimestamp()
+      .addField("Info: ", `This bot is in ${this.client.guilds.size} guilds \nCoded in discord.js \nA bot made for fun by Kuuhaku#4503`)
+      .addField("GitHub", "Check out my code on [GitHub!](https://github.com/faizal101/ThatBotJS)", true)
+      return msg.say({embed});
     };
 };
