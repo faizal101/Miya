ccc267e4040848530aa42c13b292a12b81fff3cb
『　　』 <faizal_10@live.co.uk>
2018-04-22 18:26:02 +0100

    consistency

--- src/commands/search/danbooru.js
+++ src/commands/search/danbooru.js
@@ -1,5 +1,5 @@
 const { Command } = require("discord.js-commando");
-const { MessageEmbed } = require('discord.js');
+const { MessageEmbed } = require("discord.js");
 const Danbooru = require("danbooru");
 
 module.exports = class DanCommand extends Command {
@@ -12,9 +12,9 @@ module.exports = class DanCommand extends Command {
       examples: ["danbooru kantoku"],
       args: [
         {
-          key: 'search',
-          prompt: 'Input a tag to search',
-          type: 'string'
+          key: "search",
+          prompt: "Input a tag to search",
+          type: "string"
         }
       ]
       });
@@ -22,20 +22,20 @@ module.exports = class DanCommand extends Command {
   async run (msg, {search}) {
     const embed = new MessageEmbed();
     const booru = new Danbooru();
-    const posts = await booru.posts({ tags: search, limit: 10, random:true });
+    const posts = await booru.posts({ tags: search, limit: 5, random:true });
     const post = posts[0];
     console.log(post.file_url)
     console.log(post.large_file_url);
     embed
-      .setAuthor('Danbooru', 'https://qt-anime-grils.is-serious.business/555270.png')
-      .setColor('#ec40df')
-      .setTitle('Click here to view in your browser')
+      .setAuthor("Danbooru", "https://qt-anime-grils.is-serious.business/555270.png")
+      .setColor("#ec40df")
+      .setTitle("Click here to view in your browser")
       .setURL(`https://danbooru.donmai.us/posts/${post.id}`)
       .setImage(post.large_file_url)
-      .addField('Score:', post.score, true)
-      .addField('Rating:', (post.rating === 's' ? 'safe' : post.score === 'q' ? 'questionable' : 'explicit'), true)
-      .addField('Artist:', `[${post.tag_string_artist}](https://danbooru.donmai.us/posts/?tags=${post.tag_string_artist})`, true)
-      .addField('File Fromat:', post.file_ext, true)
+      .addField("Score:", post.score, true)
+      .addField("Rating:", (post.rating === "s" ? "safe" : post.score === "q" ? "questionable" : "explicit"), true)
+      .addField("Artist:", `[${post.tag_string_artist}](https://danbooru.donmai.us/posts/?tags=${post.tag_string_artist})`, true)
+      .addField("File Fromat:", post.file_ext, true)
       .setFooter(`Post ID: ${post.id}`, this.client.user.displayAvatarURL())
     return msg.embed(embed);
   }
