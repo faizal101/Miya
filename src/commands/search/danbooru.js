a3b7e480e808132dce2a06ad78cfcab3df9ad2e6
『　　』 <faizal_10@live.co.uk>
2018-05-03 00:00:02 +0100

    improved the booru command
    catches error if more than two tags are used
    (consider donating)

--- src/commands/search/danbooru.js
+++ src/commands/search/danbooru.js
@@ -25,17 +25,21 @@ module.exports = class DanCommand extends Command {
     const booru = new Danbooru();
     const posts = await booru.posts({ tags: search, limit: 5, random:true });
     const post = posts[0];
-    embed
-      .setAuthor("Danbooru", "https://qt-anime-grils.is-serious.business/555270.png")
-      .setColor("#ec40df")
-      .setTitle("Click here to view in your browser")
-      .setURL(`https://danbooru.donmai.us/posts/${post.id}`)
-      .setImage(post.large_file_url)
-      .addField("Score:", post.score, true)
-      .addField("Rating:", (post.rating === "s" ? "safe" : post.score === "q" ? "questionable" : "explicit"), true)
-      .addField("Artist:", `[${post.tag_string_artist}](https://danbooru.donmai.us/posts/?tags=${post.tag_string_artist})`, true)
-      .addField("File Fromat:", post.file_ext, true)
-      .setFooter(`Post ID: ${post.id}`, this.client.user.displayAvatarURL())
-    return msg.embed(embed);
+    try {
+      embed
+        .setAuthor("Danbooru", "https://qt-anime-grils.is-serious.business/555270.png", "https://danbooru.donmai.us/")
+        .setColor("#ec40df")
+        .setTitle("Click here to view in your browser")
+        .setURL(`https://danbooru.donmai.us/posts/${post.id}`)
+        .setImage(post.large_file_url)
+        .addField("Score:", post.score, true)
+        .addField("Rating:", (post.rating === "s" ? "safe" : post.score === "q" ? "questionable" : "explicit"), true)
+        .addField("Artist:", `[${post.tag_string_artist}](https://danbooru.donmai.us/posts/?tags=${post.tag_string_artist})`, true)
+        .addField("File Fromat:", post.file_ext, true)
+        .setFooter(`Post ID: ${post.id}`, this.client.user.displayAvatarURL())
+        return msg.embed(embed);
+    } catch (id) {
+      msg.say("Danbooru only allows you to search with a maximum of two tags. Consider donating if you want to use more than two tags.");
+    }
   }
 };
--- src/commands/search/safebooru.js
+++ src/commands/search/safebooru.js
@@ -8,7 +8,7 @@ module.exports = class SafebooruCommand extends Command {
       name: "safebooru",
       aliases: ['sb','safe', 'sfw'],
       group: "search",
-      memberName: "sanfebooru",
+      memberName: "safebooru",
       description: "like danbooru, but sfw images",
       examples: ["safeboru kantoku"],
       args: [
@@ -25,17 +25,25 @@ module.exports = class SafebooruCommand extends Command {
     const booru = new Safebooru('https://safebooru.donmai.us/');
     const posts = await booru.posts({ tags: search, limit: 5, random:true });
     const post = posts[0];
-    embed
-      .setAuthor("Safebooru", "https://qt-anime-grils.is-serious.business/555270.png")
-      .setColor("#ec40df")
-      .setTitle("Click here to view in your browser")
-      .setURL(`https://safebooru.donmai.us/posts/${post.id}`)
-      .setImage(post.large_file_url)
-      .addField("Score:", post.score, true)
-      .addField("Rating:", "safe", true)
-      .addField("Artist:", `[${post.tag_string_artist}](https://safebooru.donmai.us/posts/?tags=${post.tag_string_artist})`, true)
-      .addField("File Fromat:", post.file_ext, true)
-      .setFooter(`Post ID: ${post.id}`, this.client.user.displayAvatarURL())
-    return msg.embed(embed);
+    if (search.includes("-rating:s") || search.includes("rating:q") || search.includes("rating:e")) {
+      msg.say("You're using safebooru. Consider using the danbooru command instead if you want lewds.");
+    } else {
+      try {
+        embed
+          .setAuthor("Safebooru", "https://qt-anime-grils.is-serious.business/555270.png", "https://safebooru.donmai.us/")
+          .setColor("#ec40df")
+          .setTitle("Click here to view in your browser")
+          .setURL(`https://safebooru.donmai.us/posts/${post.id}`)
+          .setImage(post.large_file_url)
+          .addField("Score:", post.score, true)
+          .addField("Rating:", "safe", true)
+          .addField("Artist:", `[${post.tag_string_artist}](https://safebooru.donmai.us/posts/?tags=${post.tag_string_artist})`, true)
+          .addField("File Fromat:", post.file_ext, true)
+          .setFooter(`Post ID: ${post.id}`, this.client.user.displayAvatarURL())
+          return msg.embed(embed);
+      } catch (id) {
+        msg.say("Danbooru only allows you to search with a maximum of two tags. Consider donating if you want to use more than two tags.");
+      }
+    }
   }
 };
