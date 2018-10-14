89407b514164efce40ac167cf7da6f3fc2c0d8ff
『　　』 <faizal_10@live.co.uk>
2018-10-14 18:07:20 +0100

    Finally, an avatar command!
    Hopefully, i'll try and add one command or feature to this bot every week~

--- src/commands/misc/avatar.js
+++ src/commands/misc/avatar.js
@@ -1,9 +1,51 @@
-// module.exports.run =  (bot, msg, args) => {
-//     if (msg.content.startsWith(process.env.prefix + 'avatar')){
-//       msg.channel.send(msg.author.avatarURL);
-//     }
-// };
-//
-// module.exports.help = {
-//   name: "avatar"
-// };
+const {Command} = require('discord.js-commando');
+const {MessageEmbed} = require('discord.js');
+
+module.exports = class avatarCommand extends Command {
+  constructor (client) {
+    super(client, {
+      name: 'avatar',
+      group: 'misc',
+      memberName: 'avatar',
+      description: 'Get the avatar pic of a user in the server',
+      examples: ['avatar'],
+      args: [
+        {
+          key: 'member',
+          prompt: 'You need to type in a user',
+          type: 'member',
+          default: ''
+        }
+      ]
+    });
+  }
+
+  run (msg, {member}) {
+    let user;
+    let ava;
+
+    if (member === '') {
+      user = msg.author.username;
+      ava = msg.author.displayAvatarURL({size: 1024});
+      ava = ava.replace('webp', 'png'); 
+    } else {
+      user = member.displayName;
+      ava = member.user.displayAvatarURL({size: 1024});
+      ava = ava.replace('webp', 'png'); 
+    }
+
+      
+    const embed = new MessageEmbed();
+
+    embed
+      .setAuthor(`${user}'s Avatar: `)
+      .setColor('#ec40df')
+      .setTitle('Click here to view in your browser')
+      .setURL(ava)
+      .setImage(ava);
+        
+    console.log(ava);
+
+    return msg.embed(embed);
+  }
+};
